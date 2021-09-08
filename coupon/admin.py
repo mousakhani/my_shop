61b264623f14c514f531105bc45b7ca4bf787314
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-08 10:38:04 +0430

    coupon admin add

--- coupon/admin.py
+++ coupon/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from coupon.models import Coupon
+
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+	list_display = ('code', 'discount', 'valid_from', 'valid_to', 'active')
+	list_filter = ('active', 'valid_from', 'valid_to', 'discount')
+	search_fields = ('code',)
