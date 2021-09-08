111acde02f5c76bbb4acdeb8138b66dffd4c87cb
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-08 21:27:21 +0430

    use ugettext func for translation

--- cart/forms.py
+++ cart/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-
+from django.utils.translation import ugettext_lazy as _
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
-	quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+	quantity = forms.TypedChoiceField(label=_('quantity'),choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
 	override = forms.BooleanField(widget=forms.HiddenInput, required=False, initial=False)
--- coupon/models.py
+++ coupon/models.py
@@ -1,17 +1,18 @@
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 # Create your models here.
 
 
 class Coupon(models.Model):
-	code = models.CharField(max_length=50, unique=True)
-	valid_from = models.DateTimeField()
-	valid_to = models.DateTimeField()
-	discount = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
-	active = models.BooleanField(default=True)
+	code = models.CharField(verbose_name=_('code'), max_length=50, unique=True)
+	valid_from = models.DateTimeField(verbose_name=_('valid_from'))
+	valid_to = models.DateTimeField(verbose_name=_('valid_to'))
+	discount = models.IntegerField(verbose_name=_('discount'), validators=[MaxValueValidator(100), MinValueValidator(0)])
+	active = models.BooleanField(verbose_name=_('active'), default=True)
 
 	def __str__(self):
 		return self.code
@@ -20,3 +21,7 @@ class Coupon(models.Model):
 		super(Coupon, self).clean()
 		if self.valid_to <= self.valid_from:
 			raise ValidationError('"valid to" should bigger than "valid from"')
+
+	class Meta:
+		verbose_name = _('Coupon')
+		verbose_name_plural = _('Coupons')
