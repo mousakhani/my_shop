4056ceed62a39e3f66d5e268ef984ff384ca0d21
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-08 10:37:56 +0430

    coupon model add

--- coupon/models.py
+++ coupon/models.py
@@ -1,3 +1,22 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+
 # Create your models here.
+
+
+class Coupon(models.Model):
+	code = models.CharField(max_length=50, unique=True)
+	valid_from = models.DateTimeField()
+	valid_to = models.DateTimeField()
+	discount = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
+	active = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.code
+
+	def clean(self):
+		super(Coupon, self).clean()
+		if self.valid_to <= self.valid_from:
+			raise ValidationError('"valid to" should bigger than "valid from"')
