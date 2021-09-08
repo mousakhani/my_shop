f9ae5794349ec22d567fc3a8d258329c42e64a23
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-08 20:54:54 +0430

    i18l settings configured

--- my_shop/settings.py
+++ my_shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.locale.LocaleMiddleware',  # i18l middleware
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -111,6 +113,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+LANGUAGES = (
+	('en-us', _('English')),
+	('fa-IR', _('Farsi'))
+)
+
+LANGUAGE_PATHS = (
+	'locale',
+)
 
 TIME_ZONE = 'UTC'
 
