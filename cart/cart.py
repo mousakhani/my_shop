bc68a8ccd0d84fdf463beca725c0f49d82309f2c
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-08 11:41:33 +0430

    coupon methods add to Cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from decimal import Decimal
 
+from coupon.models import Coupon
 from shop.models import Product
 
 
@@ -9,11 +10,29 @@ class Cart(object):
 	def __init__(self, request):
 		""" Initialize the cart"""
 		self.session = request.session
+		# Store current apply coupon id
+		self.coupon_id = self.session.get('coupon_id')
 		cart = self.session.get(settings.CART_SESSION_ID)
 		if not cart:
 			# save an empty cart in the session
 			cart = self.session[settings.CART_SESSION_ID] = {}
 		self.cart = cart
+
+	@property
+	def coupon(self):
+		if self.coupon_id:
+			try:
+				return Coupon.objects.get(id=self.coupon_id)
+			except Coupon.DoesNotExist:
+				pass
+		return None
+
+	def get_discount(self):
+		if self.coupon:
+			return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+
+	def get_total_price_after_discount(self):
+		return self.get_total_price() - self.get_discount()
 
 	def add(self, product, quantity, override_quantity=False):
 		"""
