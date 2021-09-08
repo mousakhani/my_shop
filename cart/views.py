9b21b5e77f28bbb5f1929969d6f362573e0ab527
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-08 11:47:32 +0430

    coupon add to cart views

--- cart/views.py
+++ cart/views.py
@@ -5,6 +5,7 @@ from django.views.decorators.http import require_POST
 
 from cart.cart import Cart
 from cart.forms import CartAddProductForm
+from coupon.forms import CouponApplyForm
 from shop.models import Product
 
 
@@ -34,4 +35,6 @@ def cart_detail(request):
 			'quantity': item['quantity'],
 			'override': True
 		})
-	return render(request, 'cart/detail.html', {'cart': cart})
+	coupon_apply_form = CouponApplyForm()
+
+	return render(request, 'cart/detail.html', {'cart': cart, 'coupon_apply_form': coupon_apply_form})
