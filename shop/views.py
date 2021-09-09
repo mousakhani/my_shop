1c7904a020e84a8fd7c49290cc0a39d377b2a397
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-09 07:45:40 +0430

    parler added to shop views

--- shop/views.py
+++ shop/views.py
@@ -11,8 +11,8 @@ def product_list(request, category_slug=None):
 	categories = Category.objects.all()
 	products = Product.objects.filter(available=True)
 	if category_slug:
-		category = get_object_or_404(Category, slug=category_slug)
-		products = products.filter(category=category)
+		category = get_object_or_404(Category, translations__slug=category_slug)
+		products = products.filter(translations__category=category)
 	return render(request, 'shop/products/list.html', {
 		'category': category,
 		'categories': categories,
@@ -22,7 +22,7 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, product_id, product_slug):
 	cart_product_form = CartAddProductForm()
-	product = get_object_or_404(Product, id=product_id, slug=product_slug, available=True)
+	product = get_object_or_404(Product, id=product_id, translations__slug=product_slug, available=True)
 	r = Recommender()
 	recommender_products = r.suggest_product_for([product], 4)
 	return render(request, 'shop/products/detail.html', {
