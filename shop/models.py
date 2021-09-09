fee5d5d48fcdc07d5fef3426b032c723d2cef550
khodemousa@gmail.com <khodemousa@gmail.com>
2021-09-09 07:44:40 +0430

    parler added to shop models

--- shop/models.py
+++ shop/models.py
@@ -2,39 +2,50 @@ from django.db import models
 
 # Create your models here.
 from django.urls import reverse
+from parler.models import TranslatableModel, TranslatedFields
+from django.utils.translation import ugettext_lazy as _
 
 
-class Category(models.Model):
-	name = models.CharField(max_length=200, db_index=True)
-	slug = models.SlugField(max_length=200, unique=True)
+class Category(TranslatableModel):
+	translations = TranslatedFields(
+		name=models.CharField(max_length=200, db_index=True),
+		slug=models.SlugField(max_length=200, unique=True)
+	)
 
+	# django-parler does not supported this code
 	class Meta:
-		ordering = ('name',)
+		verbose_name_plural = _('Categories')
+
+	# 	ordering = ('name',)
 
 	def __str__(self):
-		return self.name
+		return self.translations.name
 
 	def get_absolute_url(self):
 		return reverse('shop:product_list_by_category', args=(self.slug,))
 
 
-class Product(models.Model):
+class Product(TranslatableModel):
+	translations = TranslatedFields(
+		name=models.CharField(max_length=200, db_index=True),
+		slug=models.SlugField(max_length=200, db_index=True),
+		description=models.TextField(blank=True),
+
+	)
 	category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
-	name = models.CharField(max_length=200, db_index=True)
-	slug = models.SlugField(max_length=200, db_index=True)
-	image = models.ImageField(upload_to='products/%y/%m/%d', blank=True)
-	description = models.TextField(blank=True)
 	price = models.DecimalField(max_digits=20, decimal_places=2)
+	image = models.ImageField(upload_to='products/%y/%m/%d', blank=True)
 	available = models.BooleanField(default=True)
 	created = models.DateTimeField(auto_now_add=True)
 	updated = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		ordering = ('name',)
-		index_together = (('id', 'slug'),)
+	# django-parler does not supported this codes
+	# class Meta:
+	# 	ordering = ('name',)
+	# 	index_together = (('id', 'slug'),)
 
 	def __str__(self):
-		return self.name
+		return self.translations.name
 
 	def get_absolute_url(self):
 		return reverse('shop:product_detail', args=(self.id, self.slug))
